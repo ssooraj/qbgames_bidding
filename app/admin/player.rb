0e2f9831813129b1b787ea295706d470280743a7
soorajs <soorajs@qburst.com>
2015-11-27 18:25:16 +0530

    admin site

--- app/admin/player.rb
+++ app/admin/player.rb
@@ -12,10 +12,26 @@ ActiveAdmin.register Player do
             row :last_name
             row :is_sold
             row :team
+            row :is_cricket
+            row :is_football
+            row :is_badminton
+            row :base_price
             row('image') { image_tag resource.avatar.thumb.url rescue nil}
           end
         end
       end
     end
+  end
+
+  index do
+    id_column
+    column :first_name
+    column :last_name
+    column :is_sold
+    column :is_cricket
+    column :is_football
+    column :is_badminton
+    column :base_price
+    actions
   end
 end
